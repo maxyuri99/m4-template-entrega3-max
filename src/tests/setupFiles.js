38694f7fbc1718fd99119f6eb5d569e38876e2bb
Max Yuri Ferreira dos Santos <maxyurisantos@hotmail.com>
2024-01-14 13:59:56 -0300

    Retirando pasta tests

--- src/tests/setupFiles.js
+++
@@ -1,4 +0,0 @@
-import { app } from "../app";
-import supertest from "supertest";
-
-export const request = supertest(app);
--- src/tests/utils/errorDefaultExpects.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "vitest";
-
-export const errorDefaultExpects = (data) => {
-   expect(data).toBeTypeOf("object");
-
-   expect(data.error).toBeDefined();
-   expect(data.error).toBeTypeOf("string");
-};
